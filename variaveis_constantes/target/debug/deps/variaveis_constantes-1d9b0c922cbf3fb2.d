4ed43337f45de33c55bda2cdef1efe13a9c6928d
Mateus de Souza <mathrews>
2024-04-11 15:51:25 -0300

    variaveis e constantes

---
+++ variaveis_constantes/target/debug/deps/variaveis_constantes-1d9b0c922cbf3fb2.d
@@ -0,0 +1,5 @@
+/home/matedev/My-projects/learning_rust/variaveis_constantes/target/debug/deps/libvariaveis_constantes-1d9b0c922cbf3fb2.rmeta: src/main.rs
+
+/home/matedev/My-projects/learning_rust/variaveis_constantes/target/debug/deps/variaveis_constantes-1d9b0c922cbf3fb2.d: src/main.rs
+
+src/main.rs:
