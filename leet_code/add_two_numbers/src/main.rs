1a87c285d6bc3ca5091894f2845abb78e980390c
Mateus de Souza <mathrews>
2024-04-15 17:12:14 -0300

    implementando parte da solução add2numbers leetcode

--- leet_code/add_two_numbers/src/main.rs
+++ leet_code/add_two_numbers/src/main.rs
@@ -13,12 +13,15 @@ impl ListNode {
 
 fn main() {}
 
-fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {}
+fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    {
+        let mut new_node = None;
+        let mut original_node = l1;
 
-#[test]
-fn test_first() {
-    let input1 = [2, 4, 3];
-    let input2 = [5, 6, 4];
-
-    assert_eq!(add_two_numbers(input1, input2), [7, 0, 8]);
+        while let Some(mut node) = original_node {
+            original_node = node.next; // advancing the current node
+            node.next = new_node;
+            new_node = Some(node);
+        }
+    }
 }
