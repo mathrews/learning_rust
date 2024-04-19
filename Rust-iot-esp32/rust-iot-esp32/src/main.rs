b473e6ad605776d96b90107afa89a5d490767ab8
mathrews <mateusps4gg@gmail.com>
2024-04-19 14:52:55 -0300

    Errors fixed Rust-iot. ToDo: testar no esp-32

--- Rust-iot-esp32/rust-iot-esp32/src/main.rs
+++ Rust-iot-esp32/rust-iot-esp32/src/main.rs
@@ -1,12 +1,18 @@
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    netif::{EspNetif, NetifStack},
+    hal::{
+        gpio,
+        peripherals::Peripherals,
+        uart::{config, UartDriver},
+        units::Hertz,
+    },
     nvs::EspDefaultNvsPartition,
-    wifi::{ClientConfiguration, Configuration, EspWifi},
+    wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 use heapless::String;
 
-fn main() {
+#[allow(unused_variables, unused_mut)]
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -19,19 +25,49 @@ fn main() {
     let wifi_ssid: String<32> = String::try_from("IFCE NASH 5G").expect("INVALID SSID");
     let wifi_pass: String<64> = String::try_from("nashifce8556").expect("INVALID PASSWORD");
 
-    let nvs = EspDefaultNvsPartition::take().unwrap();
+    // Peripherals
+    let peripherals = Peripherals::take().unwrap();
+    let sysloop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
 
-    let mut wifi = EspWifi::new(
-        EspNetif::new(NetifStack::Sta).unwrap(),
-        EspSystemEventLoop::take().unwrap(),
-        Some(nvs),
+    // Configure UART
+    // Create handle for UART config struct
+    let config = config::Config::default().baudrate(Hertz(115_200));
+
+    // Instantiate UART
+    let mut uart = UartDriver::new(
+        peripherals.uart0,
+        peripherals.pins.gpio21,
+        peripherals.pins.gpio20,
+        Option::<gpio::Gpio0>::None,
+        Option::<gpio::Gpio1>::None,
+        &config,
     )
     .unwrap();
 
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs))?,
+        sysloop,
+    )?;
+
     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
         ssid: wifi_ssid,
+        bssid: None,
+        auth_method: AuthMethod::None,
         password: wifi_pass,
-        ..Default::default()
-    }))
-    .unwrap();
+        channel: None,
+    }))?;
+
+    // Start Wifi:
+    wifi.start()?;
+
+    // Connect Wifi:
+    wifi.connect()?;
+
+    // Wait until the network interface is up
+    wifi.wait_netif_up()?;
+
+    println!("Wifi Connected");
+
+    loop {}
 }
