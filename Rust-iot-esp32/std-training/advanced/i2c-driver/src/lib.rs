16e8bc0d2c65b12a98de66489406bd7aa02c2fe3
mathrews <mateusps4gg@gmail.com>
2024-04-29 17:54:00 -0300

    some changes

--- Rust-iot-esp32/std-training/advanced/i2c-driver/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![deny(unsafe_code)]
-#![no_std]
-
-// Uncomment the following line to run the solution, check main.rs for further instructions
-pub mod icm42670p_solution;
-
-// Comment out the following line to run the solution, check main.rs for further instructions
-// pub mod icm42670p;
