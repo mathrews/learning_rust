cc2138101200d2f5f7788c6c60601459d03c07ab
mathrews <mateusps4gg@gmail.com>
2024-05-20 15:11:26 -0300

    learning about vectors

--- Basic_notions/basic_notions/src/main.rs
+++ Basic_notions/basic_notions/src/main.rs
@@ -194,5 +194,35 @@ fn main() {
     };
     println!("{last}");
 
-    let last = *slice.get(5).unwrap_or(&-1);
+    let _last = *slice.get(5).unwrap_or(&-1);
+
+    // Vec1
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v.push(30);
+
+    let first = v[0]; // will panic if out-of-range
+    let maybe_first = v.get(0);
+    
+    println!("v is {:?}", v);
+    println!("first is {}", first);
+    println!("maybe_first is {:?}", maybe_first);
+    println!();
+
+    // Vec2
+    fn dump(arr: &[i32]) {
+        println!("arr is {:?}", arr);
+    }
+
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v.push(30);
+    
+    dump(&v);
+
+    let slice = &v[1..];
+    println!("slice is {:?}", slice);
+    println!();
 }
